08fec4ae6f04b3405995763c355422a166cc64c5
BuildTools <unconfigured@null.spigotmc.org>
2018-10-28 22:53:46 -0600

    Moved BigSpaces elements to own dir /game/

--- src/mdj2/bigspace/LauncherMain.java
+++ src/mdj2/bigspace/LauncherMain.java
@@ -1,8 +1,15 @@
 package mdj2.bigspace;
 
+import mdj2.bigspace.engine.GameEngine;
+import mdj2.bigspace.game.core.BigSpaceCore;
+import mdj2.bigspace.test.Picazzo;
+
 public class LauncherMain {
 
 	public static void main(String[] args) {
 		System.out.println("Launching Game...");
+		GameEngine engine = new GameEngine(new Picazzo());
+		engine.setDebugMode(true);
+		engine.start(); 
 	}
 }
