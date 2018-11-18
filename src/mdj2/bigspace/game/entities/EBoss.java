59caabbb8156525dbf5ed7a77425940330d705f2
BuildTools <unconfigured@null.spigotmc.org>
2018-11-17 19:16:48 -0600

    Resolved Few Enemy Issues

--- src/mdj2/bigspace/game/entities/EBoss.java
+++ src/mdj2/bigspace/game/entities/EBoss.java
@@ -1,6 +1,13 @@
 package mdj2.bigspace.game.entities;
 
+import mdj2.bigspace.engine.graphics.GameWorld;
+
 public class EBoss extends EnemyBase {
+
+	public EBoss(GameWorld world) {
+		super(world);
+		// TODO Auto-generated constructor stub
+	}
 
 	
 	
--- src/mdj2/bigspace/game/entities/EnemyBase.java
+++ src/mdj2/bigspace/game/entities/EnemyBase.java
@@ -1,7 +1,13 @@
 package mdj2.bigspace.game.entities;
 
 import mdj2.bigspace.engine.gameobjects.GameObject;
+import mdj2.bigspace.engine.graphics.GameWorld;
 
 public abstract class EnemyBase extends GameObject {
+
+	public EnemyBase(GameWorld world) {
+		super(world);
+		// TODO Auto-generated constructor stub
+	}
 
 }
